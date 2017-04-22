e055b4b01ac96b0ae61acdf337c05ae02e68a68f
hlimbo <limboh27@gmail.com>
2017-04-21 23:58:32 -0700

    More prototyping

--- EDGELORD/Assets/CutPrototype/CutProto.cs
+++ EDGELORD/Assets/CutPrototype/CutProto.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Script illustrates where an object is sliced using Debug.DrawLine
 public class CutProto : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject Watermelon;
+    public float sliceDistance = 500.0f;
+
+    [SerializeField]
+    List<SpriteSlicer2DSliceInfo> slicedObjectInfo = null;
+
+    public bool canDeleteParent = false;
+  
+    void Start()
+    {
+        Vector3 endPosition = transform.position + (Vector3.right * sliceDistance);
+        //SpriteSlicer2D.SliceAllSprites(Watermelon.transform.position, endPosition);
+        //Debug.DrawLine(transform.position, endPosition, Color.red, 120.0f);
+        SpriteSlicer2D.SliceSprite(transform.position, endPosition, Watermelon,canDeleteParent,ref slicedObjectInfo);
+        //SpriteSlicer2D.ExplodeSprite(Watermelon, 10, 10.0f);
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.DrawLine(transform.position, mousePos, Color.red);
+        }
+    }
+  
 }
