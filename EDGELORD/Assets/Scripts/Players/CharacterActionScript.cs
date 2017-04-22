50a81f9efe20d73a5c56a51ddc9ad241e0446d18
verythinice <quake.grizzly@gmail.com>
2017-04-22 13:44:58 -0700

    Started work on character controller

---
+++ EDGELORD/Assets/Scripts/Players/CharacterActionScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterActionScript : MonoBehaviour {
+    PlayerInputManager inputs;
+
+	// Use this for initialization
+	void Start () {
+        inputs = GetComponent<PlayerInputManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (inputs.getActionDown()) {
+            //Do Something
+        }
+	}
+}
