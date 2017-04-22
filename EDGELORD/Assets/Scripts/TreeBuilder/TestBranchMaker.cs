5bafecb9910d7a5bba4f58bbdf2705f4e219edc7
Michael Wolf <michaelwolf95@gmail.com>
2017-04-21 22:57:30 -0700

    Project structure update

---
+++ EDGELORD/Assets/Scripts/TreeBuilder/TestBranchMaker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EDGELORD
+{
+	public class TestBranchMaker : MonoBehaviour
+	{
+	    public Vector2 ShootDirection;
+	    public float Length;
+	    public KeyCode TestInput = KeyCode.Space;
+
+        
+
+
+	}
+}
