dea362b2520894ba518fcf193a50dfe7e3f13c22
VinCMG <vincentchang9999@gmail.com>
2017-04-23 10:48:42 -0700

    Timer display hooks added to game manager

--- EDGELORD/Assets/Scripts/GameManager.cs
+++ EDGELORD/Assets/Scripts/GameManager.cs
@@ -11,7 +11,12 @@ namespace EDGELORD.Manager {
 
         private ScoreDisplay player1ScoreDisplay;
         private ScoreDisplay player2ScoreDisplay;
+        private TimerDisplay timerDisplay;
 
+        private bool isGameOver;
+        private float timeLeft;
+
+        public int gameLengthInSeconds = 60;
         public bool DEBUG_Disable_Music = false;
 
         /* game field and other objects go here */
@@ -19,6 +24,7 @@ namespace EDGELORD.Manager {
         protected GameManager () {}
 
         void Awake () {
+            isGameOver = true;
             musicPlayer = (MusicPlayer)FindObjectOfType(typeof(MusicPlayer));
             sfxPlayer = (SfxPlayer)FindObjectOfType(typeof(SfxPlayer));
             ui = (Canvas)FindObjectOfType(typeof(Canvas));
@@ -26,6 +32,9 @@ namespace EDGELORD.Manager {
             Component[] playerScoreDisplays = ui.GetComponentsInChildren<ScoreDisplay>();
             player1ScoreDisplay = (ScoreDisplay)playerScoreDisplays[0];
             player2ScoreDisplay = (ScoreDisplay)playerScoreDisplays[1];
+            timerDisplay = ui.GetComponentInChildren<TimerDisplay>();
+            timeLeft = gameLengthInSeconds;
+            timerDisplay.UpdateTime(gameLengthInSeconds);
 
             InitObjects();
             StartGame();
@@ -36,28 +45,37 @@ namespace EDGELORD.Manager {
         }
 
         void StartGame () {
+            if (!isGameOver) {
+                return;
+            }
+
             if (!DEBUG_Disable_Music) {
                 musicPlayer.StartMusic();
             }
 
-            // // run countdown
-            // Countdown countdown = ui.GetComponentInChildren<Countdown>();
-            // if (countdown != null) {
-            //     countdown.StartCountdown();
-            // } else {
-            //     Debug.Log("countdown is null");
-            // }
-
-            // allow player control
-
-            // DEBUG
+            isGameOver = false;
+            StartCoroutine(startTimer());
             StartCoroutine(TEST_updateScore());
+        }
+
+        void StopGame () {
+
         }
 
         public void UpdateScores () {
             // TODO: calculate scores for each player here
             player1ScoreDisplay.UpdateScore(Random.value);
             player2ScoreDisplay.UpdateScore(Random.value);
+        }
+
+        private IEnumerator startTimer() {
+            while (timeLeft > 0) {
+                timeLeft -= Time.deltaTime;
+                timerDisplay.UpdateTime(timeLeft);
+                yield return null;
+            }
+            timerDisplay.UpdateTime(0);
+            StopGame();
         }
 
         private IEnumerator TEST_updateScore() {
---
+++ EDGELORD/Assets/Scripts/UI/TimerDisplay.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using TMPro;
+using System;
+
+public class TimerDisplay : MonoBehaviour {
+    private TMP_Text textBox;
+
+    void Awake () {
+        textBox = GetComponent<TMP_Text>();
+    }
+
+    public void UpdateTime(float timeLeftInSeconds) {
+        int mins = (int)(timeLeftInSeconds / 60);
+        float secs = (timeLeftInSeconds % 60);
+        textBox.text = string.Format("{0}:{1,00:00.0}", mins, secs);
+    }
+}
