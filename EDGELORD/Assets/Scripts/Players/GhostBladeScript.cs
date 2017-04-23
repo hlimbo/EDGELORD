032eb36bfc2a554353f800c86f8ea674c0c805b9
verythinice <quake.grizzly@gmail.com>
2017-04-22 20:02:08 -0700

    Added preview ghost blade
    also bug fixes

---
+++ EDGELORD/Assets/Scripts/Players/GhostBladeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostBladeScript : MonoBehaviour {
+
+    public float defaultLength;
+    public float defaultWidth;
+
+	public void setScale(Vector2 scale) {
+        transform.localScale = new Vector3(scale.x*defaultWidth, scale.y*defaultLength, 1);
+    }
+
+    public void setRotation(Vector2 rotation) {
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, rotation);
+    }
+}
