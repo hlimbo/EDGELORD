c1eb222736a7f39c7dffad3a5169fc6af6e78d5e
Jake Smith <jesmith24@comcast.net>
2017-04-23 20:10:45 -0700

    Signwoosh script

---
+++ EDGELORD/Assets/Scripts/UI/PlaySignWoosh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlaySignWoosh : MonoBehaviour {
+
+    public AudioClip wood;
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void playWoosh()
+    {
+        audioSource.pitch += (Random.value - 0.5f) * 0.5f;
+        audioSource.Play();
+    }
+}
