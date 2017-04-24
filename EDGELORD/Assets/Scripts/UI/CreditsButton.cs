324a25d4fb13e949b6b50bc8b3ee61eb4a7e7c85
Michael Wolf <michaelwolf95@gmail.com>
2017-04-23 20:15:54 -0700

    Canvas Changes. Fixed Building

--- EDGELORD/Assets/Scripts/UI/CreditsButton.cs
+++ EDGELORD/Assets/Scripts/UI/CreditsButton.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
-using UnityEditor.SceneManagement;
+//using UnityEditor.SceneManagement;
 
 public class CreditsButton : MonoBehaviour {
     private Button button;
