7cdb9b0e7317253d764eb67781a8384201604b31
Michael Wolf <michaelwolf95@gmail.com>
2017-04-23 17:53:38 -0700

    Added start flash to player swords

---
+++ EDGELORD/Assets/ColorFlashUntilGameStart.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Players;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace EDGELORD
+{
+	public class ColorFlashUntilGameStart : MonoBehaviour
+	{
+	    public PlayerID playerID;
+        public CharacterActionScript myCharacter;
+	    public Renderer myRenderer;
+	    public Color color1;
+	    public Color color2;
+	    public UnityEvent OnPlayerReady;
+
+	    void Start()
+	    {
+	        var characters = FindObjectsOfType<CharacterActionScript>();
+	        foreach (CharacterActionScript c in characters)
+	        {
+                if (c.OwningPlayer == playerID)
+                {
+                    myCharacter = c;
+                    break;
+                }
+            }
+	        if (myCharacter != null)
+	        {
+	            StartCoroutine(CoWaitForReady());
+	        }
+	    }
+
+	    IEnumerator CoWaitForReady()
+	    {
+            yield return new WaitForEndOfFrame();
+            float timer = 0f;
+	        Color startColor = myRenderer.material.color;
+            while (!myCharacter.isReady)
+            {
+                timer += Time.deltaTime;
+                myRenderer.material.color = Color.Lerp(color1, color2, (Mathf.Sin(Mathf.PI * 2 * timer)/2f) + 0.5f);
+	            yield return null;
+	        }
+	        myRenderer.material.color = startColor;
+            OnPlayerReady.Invoke();
+	    }
+	}
+}
